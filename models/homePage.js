c4cf4ae303a54e7bdf16f017a90ed538709aa83a
Nishanth v <100022076+nishanth-code@users.noreply.github.com>
2023-03-22 11:36:24 +0000

    models created

---
+++ models/homePage.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const homePageSchema = new mongoose.Schema({
+    carouselImages:[String],
+    categories:[{
+        url:String,
+        name:String
+
+    }],
+    ocassions:[String],
+    discountBanner:String,
+    offers:[String],
+    footerContent:{
+        type:String,
+        default:`
+        If you would like to experience the best of online shopping for men, women and kids in India, you are at the right place. Myntra is the ultimate destination for fashion and lifestyle, being host to a wide array of merchandise including clothing, footwear, accessories, jewellery, personal care products and more. It is time to redefine your style statement with our treasure-trove of trendy items. Our online store brings you the latest in designer products straight out of fashion houses. You can shop online at Myntra from the comfort of your home and get your favourites delivered right to your doorstep.
+        BEST ONLINE SHOPPING SITE IN INDIA FOR FASHION
+        Be it clothing, footwear or accessories, Myntra offers you the ideal combination of fashion and functionality for men, women and kids. You will realise that the sky is the limit when it comes to the types of outfits that you can purchase for different occasions.
+        Smart men’s clothing - At Myntra you will find myriad options in smart formal shirts and trousers, cool T-shirts and jeans, or kurta and pyjama combinations for men. Wear your attitude with printed T-shirts. Create the back-to-campus vibe with varsity T-shirts and distressed jeans. Be it gingham, buffalo, or window-pane style, checked shirts are unbeatably smart. Team them up with chinos, cuffed jeans or cropped trousers for a smart casual look. Opt for a stylish layered look with biker jackets. Head out in cloudy weather with courage in water-resistant jackets. Browse through our innerwear section to find supportive garments which would keep you confident in any outfit.
+        Trendy women’s clothing - Online shopping for women at Myntra is a mood-elevating experience. Look hip and stay comfortable with chinos and printed shorts this summer. Look hot on your date dressed in a little black dress, or opt for red dresses for a sassy vibe. Striped dresses and T-shirts represent the classic spirit of nautical fashion. Choose your favourites from among Bardot, off-shoulder, shirt-style, blouson, embroidered and peplum tops, to name a few. Team them up with skinny-fit jeans, skirts or palazzos. Kurtis and jeans make the perfect fusion-wear combination for the cool urbanite. Our grand sarees and lehenga-choli selections are perfect to make an impression at big social events such as weddings. Our salwar-kameez sets, kurtas and Patiala suits make comfortable options for regular wear.`
+
+
+    }
+
+})
+
+
+const homeModel = mongoose.model('homePage',HomeSchema)
+module.exports = homeModel;
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require('express');
+const hompageSchema = require('./models/homePage')
+const app = express();
+const methodOverride = require('method-override')
+const path = require('path')
+const ejsMate = require('ejs-mate')
+
+// const bodyParser = require('body-parser')
+
+const connectDB = require('./config/db');
+app.set('views',path.join(__dirname,'./views'))
+const PORT =5000 ;
+// process.env.PORT;
+
+
+connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine','ejs')
+app.use(methodOverride('_method'))
+
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`)
+);
