607c152ae12afc51568bd61d96f9a02fe6a6881f
Nishanth v <nishanthnavneeth200@gmail.com>
2023-03-24 17:44:32 +0530

    create update  retrive routes set

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const hompageSchema = require('./models/homePage')
+const hompage = require('./models/homePage')
 const app = express();
 const methodOverride = require('method-override')
 const path = require('path')
@@ -18,6 +18,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine','ejs')
 app.use(methodOverride('_method'))
+
+app.get('/home',async(req,res)=>{
+  const home =await  homepage.find()
+  res.send({home})
+})
+app.post('/home',async(req,res)=>{
+  const {carouselImages,categoriesUrl,categoriesName,ocassions,discountBanner,offers,footerContent} = req.body;
+  const categories = []
+  for(let i=0;i<categoriesUrl.length;i++){
+    categories.push({url:`${categoriesUrl[i]}`,name:`${categoriesName[i]}`});
+
+  }
+  const newhomepage = new homepage({carouselImages,categories,ocassions,discountBanner,offers,footerContent})
+ await newhomepage.save()
+})
+app.put('/home/edit',async(req,res)=>{
+  const {carouselImages,categoriesUrl,categoriesName,ocassions,discountBanner,offers,footerContent} = req.body;
+  const categories = []
+  for(let i=0;i<categoriesUrl.length;i++){
+    categories.push({url:`${categoriesUrl[i]}`,name:`${categoriesName[i]}`});
+
+  }
+  const newhomepage = homepage.findOneAndUpdate({carouselImages,categories,ocassions,discountBanner,offers,footerContent})
+})
+
+
 
 app.listen(PORT, () =>
   console.log(`Server started on port ${PORT}`)
